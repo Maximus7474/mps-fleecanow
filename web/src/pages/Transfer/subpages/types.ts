63b84b6007eadcc8fb61952fa6e8d878d62fa2f5
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-27 01:04:00 +0200

    feat(Transfer/types): Proximity Profile interface

--- web/src/pages/Transfer/subpages/types.ts
+++ web/src/pages/Transfer/subpages/types.ts
@@ -1,5 +1,11 @@
+import { UserSharedProfile } from 'src/types';
+
 export type transferAction = 'home' | 'username' | 'proximity';
 
 export interface TransferProps {
   setSection: (section: transferAction) => void;
+}
+
+export interface ProximityShareProfile extends UserSharedProfile {
+  distance: number;
 }
