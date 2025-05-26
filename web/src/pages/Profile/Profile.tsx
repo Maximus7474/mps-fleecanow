16fc2fcc9092d1501a94f246abf86d824e34c0d1
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-26 21:18:00 +0200

    feat(Profile): editing profile view

--- web/src/pages/Profile/Profile.tsx
+++ web/src/pages/Profile/Profile.tsx
@@ -1,14 +1,28 @@
-import { LogOut, PenLine, Trash } from 'lucide-react';
+import { LogOut, PenLine, Save, Trash } from 'lucide-react';
 import { useAuth } from '../../hooks/useAuth';
 import './Profile.css';
 import { devMode } from '../../utils/utils';
 import { Navigate } from 'react-router-dom';
 import ProfilePicture from '../../components/ProfilePicture';
+import { useState } from 'react';
+import { fetchNui } from '../../utils/fetchNui';
 
 type DeletionResponse = { success: true } | { success: false; error: string };
+interface editProfileError {
+  avatar?: string;
+  displayName?: string;
+  email?: string;
+  username?: string
+}
 
 const ProfilePage: React.FC = () => {
-  const { user, logout } = useAuth();
+  const { user, logout, updateUser } = useAuth();
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [displayName, setDisplayName] = useState<string>(user?.displayName || '');
+  const [email, setEmail] = useState<string>(user?.email || '');
+  const [username, setUsername] = useState<string>(user?.username || '');
+  const [avatar, setAvatar] = useState<string>(user?.avatar || '');
+  const [errors, setErrors] = useState<editProfileError>({});
 
   const handleDelete = () => {
     if (devMode) {
@@ -50,6 +64,62 @@ const ProfilePage: React.FC = () => {
     });
   };
 
+  const handleSave = () => {
+
+    let invalidData = false;
+    const newData = {
+      ...user,
+    };
+    const errors: editProfileError = {};
+    
+    const urlRegex = /^https?:\/\/[^\s/$.?#].[^\s]*$/i;
+    if (avatar.length > 20 && urlRegex.test(avatar)) {
+      newData.avatar = avatar;
+    } else {
+      invalidData = true;
+      errors.avatar = 'Avatar image URL is invalid';
+    }
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email.length > 10 && emailRegex.test(avatar)) {
+      newData.email = email;
+    } else {
+      invalidData = true;
+      errors.email = 'Email address is invalid';
+    }
+
+    setErrors(errors);
+
+    if (devMode) {
+      alert('Simulated data being saved');
+      setIsEditing(false);
+    }
+
+    updateUser({
+      uuid: user!.uuid, // Woops
+      avatar: avatar ?? user?.avatar,
+      displayName,
+      email,
+      username,
+    })
+    .then(r => {
+      if (r) {
+        setIsEditing(false);
+      } else {
+        // ToDo: propagate to UI
+        console.error('unable to update');
+      }
+    });
+  }
+
+  const profilePicSource = (temporary: string, current: string, fallback: string): string => {
+    const imageUrlRegex = /^https?:\/\/.+\.(jpg|jpeg|png|gif|webp|bmp|svg)(\?.*)?$/i;
+
+    if (imageUrlRegex.test(temporary)) return temporary;
+    if (current?.trim()) return current;
+    return fallback;
+  };
+
   if (!user) {
     return <Navigate to='/' replace />;
   }
@@ -63,27 +133,87 @@ const ProfilePage: React.FC = () => {
         <div className='profile'>
           <ProfilePicture 
             className='avatar'
-            src={user.avatar || '/icon.png'}
+            src={profilePicSource(avatar ?? '', user.avatar ?? '', '/icon.png')}
             alt={`${user.username}'s avatar`}
             fallback='/icon.png'
           />
-          <h2 className='display-name'>{user.displayName || user.username}</h2>
-          <p className='username'>@{user.username}</p>
-          {user?.email && <p className='email'>{user.email}</p>}
+          {isEditing ? (
+            <div className='profile-edit'>
+              <div>
+                <label htmlFor='avatar'>Avatar</label>
+                <input
+                  type='url'
+                  name='avatar'
+                  value={avatar ?? ''}
+                  placeholder='www.imagehost.com/image.png'
+                  onChange={(e) => setAvatar(e.target.value)}
+                />
+                <p className='error'>{errors.username}</p>
+              </div>
+              <div>
+                <label htmlFor='username'>Username</label>
+                <input
+                  type='text'
+                  name='username'
+                  value={username}
+                  placeholder='Display name'
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <p className='error'>{errors.username}</p>
+              </div>
+              <div>
+                <label htmlFor='displayname'>Display Name</label>
+                <input
+                  type='text'
+                  name='displayname'
+                  value={displayName}
+                  placeholder='Display name'
+                  onChange={(e) => setDisplayName(e.target.value)}
+                />
+                <p className='error'>{errors.displayName}</p>
+              </div>
+              <div>
+                <label htmlFor='email'>Email</label>
+                <input
+                  type='email'
+                  name='email'
+                  value={email}
+                  placeholder='Email (optional)'
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p className='error'>{errors.email}</p>
+              </div>
+            </div>
+          ) : (
+            <>
+              <h2 className='display-name'>{user.displayName || user.username}</h2>
+              <p className='username'>@{user.username}</p>
+              {user.email && <p className='email'>{user.email}</p>}
+            </>
+          )}
         </div>
         <div className='actions'>
-          <button onClick={logout} className='destructive'>
-            <LogOut />
-            <p>Log Out</p>
-          </button>
-          <button onClick={() => {}} className='edit'>
-            <PenLine />
-            <p>Edit Profile</p>
-          </button>
-          <button onClick={handleDelete} className='destructive'>
-            <Trash />
-            <p>Delete Profile</p>
-          </button>
+          {
+            isEditing ? (
+              <button onClick={handleSave} className='edit'>
+                <Save />
+                <p>Save</p>
+              </button>
+            ) : (<>
+              <button onClick={logout} className='destructive'>
+                <LogOut />
+                <p>Log Out</p>
+              </button>
+              <button onClick={() => setIsEditing(true)} className='edit'>
+                <PenLine />
+                <p>Edit Profile</p>
+              </button>
+              <button onClick={handleDelete} className='destructive'>
+                <Trash />
+                <p>Delete Profile</p>
+              </button>
+            </>)
+          }
         </div>
       </div>
     </div>
