1875a007ebe16cff0d374c958736ba427242ab1f
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-27 01:04:27 +0200

    feat(Transfer): created a debug for env browser list of users for proximity sharing

---
+++ web/src/pages/Transfer/subpages/debug.ts
@@ -0,0 +1,60 @@
+import { ProximityShareProfile } from './types';
+
+export const proximityShareProfiles: ProximityShareProfile[] = [
+  {
+    username: 'sunny_traveler',
+    displayName: 'Ava',
+    avatar: 'https://avatar.iran.liara.run/public/61',
+    distance: 25,
+  },
+  {
+    username: 'code_monkey',
+    displayName: 'Devon',
+    avatar: 'https://avatar.iran.liara.run/public/13',
+    distance: 12,
+  },
+  {
+    username: 'mysterycat47',
+    avatar: 'https://avatar.iran.liara.run/public/63',
+    distance: 40,
+  },
+  {
+    username: 'bike_explorer',
+    displayName: 'Riley',
+    distance: 60,
+  },
+  {
+    username: 'artsy_nova',
+    displayName: 'Nova',
+    avatar: 'https://avatar.iran.liara.run/public/58',
+    distance: 18,
+  },
+  {
+    username: 'quietfox99',
+    distance: 5,
+  },
+  {
+    username: 'chess_master_91',
+    displayName: 'Elias',
+    avatar: 'https://avatar.iran.liara.run/public/40',
+    distance: 33,
+  },
+  {
+    username: 'zen_gardener',
+    displayName: 'Liam',
+    avatar: 'https://avatar.iran.liara.run/public/30',
+    distance: 22,
+  },
+  {
+    username: 'starryskies',
+    displayName: 'Zoe',
+    avatar: 'https://avatar.iran.liara.run/public/78',
+    distance: 11,
+  },
+  {
+    username: 'booklover_jane',
+    displayName: 'Jane',
+    avatar: 'https://avatar.iran.liara.run/public/85',
+    distance: 9,
+  },
+];
