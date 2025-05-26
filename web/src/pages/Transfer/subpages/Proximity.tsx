e5e47d08a6ac6bccd6a42a80b9f24986f02835c6
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-27 01:04:50 +0200

    feat(Transfer): initial selection menu to share money via proximity

--- web/src/pages/Transfer/subpages/Proximity.tsx
+++ web/src/pages/Transfer/subpages/Proximity.tsx
@@ -1,13 +1,48 @@
 import { Undo2 } from 'lucide-react';
-import { TransferProps } from './types';
+import { ProximityShareProfile, TransferProps } from './types';
+import { useEffect, useState } from 'react';
+import { fetchNui } from '../../../utils/fetchNui';
+import { proximityShareProfiles } from './debug';
+import ProfilePicture from '../../../components/ProfilePicture';
 
 const ProximuityTransfer: React.FC<TransferProps> = ({ setSection }) => {
+  const [proximityUsers, setProximityUsers] = useState<ProximityShareProfile[]>([]);
+
+  useEffect(() => {
+    fetchNui<ProximityShareProfile[]>('fleecanow:getcloseplayers', {}, proximityShareProfiles).then((resp) => {
+      setProximityUsers(resp);
+    });
+  }, []);
+
   return (
-    <>
-      <button onClick={() => setSection('home')}>
+    <div>
+      <button onClick={() => setSection('home')} className='return'>
         <Undo2 />
       </button>
-    </>
+      <div className='closeby-users'>
+        <p>Select a user to send money to</p>
+        <div>
+          {
+            proximityUsers.map((user, i) => (
+              <div key={i} className='card'>
+                <ProfilePicture
+                  src={user.avatar ?? '/icon.png'}
+                  fallback='/icon.png'
+                  className=''
+                />
+                <div>
+                  { user.displayName && <p>{user.displayName}</p> }
+                  <p>{user.username}</p>
+                </div>
+                <div>
+                  {user.distance}m
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
   );
 };
 
