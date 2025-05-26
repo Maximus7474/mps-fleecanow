751edbe1c9453acf160424db69e345d436ec50a9
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-26 23:49:30 +0200

    refactor(AuthProvider): add initial effect to get if player is connected to avoid redundant logging in

--- web/src/components/provider/AuthProvider.tsx
+++ web/src/components/provider/AuthProvider.tsx
@@ -1,14 +1,30 @@
-import { useState, ReactNode } from 'react';
+import { useState, ReactNode, useEffect } from 'react';
 import { fetchNui } from '../../utils/fetchNui';
 import { User, LoginResponse, UpdateProfileResponse } from '../../types';
 import { AuthContext } from '../../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
+
+const mockAccount: User = {
+  uuid: '17fecfcd-c694-41a3-a647-118ccf7c6bed',
+  username: 'maximusprime',
+  displayName: 'Maximus Prime',
+  email: 'maximus.prime@lbscripts.com',
+  avatar: 'https://avatars.githubusercontent.com/u/94017712?v=4',
+};
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const navigate = useNavigate();
   const [user, setUser] = useState<User | null>(null);
   const [loginError, setLoginError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchNui<User | null>('fleecanow:getconnectedaccount', {}, mockAccount).then((user) => {
+      if (!user) return;
+
+      setUser(user);
+    });
+  }, []);
 
   async function login(username: string, password: string) {
     setLoading(true);
@@ -18,13 +34,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       { username, password },
       {
         success: true,
-        user: {
-          uuid: '17fecfcd-c694-41a3-a647-118ccf7c6bed',
-          username: 'maximusprime',
-          displayName: 'Maximus Prime',
-          email: 'maximus.prime@lbscripts.com',
-          avatar: 'https://avatars.githubusercontent.com/u/94017712?v=4',
-        },
+        user: mockAccount,
       },
     ).then((data) => {
       if (data.success) {
