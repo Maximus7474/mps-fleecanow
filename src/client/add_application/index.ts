e64be2471b28c35d079918ea0012526f1e19df1b
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-28 11:59:33 +0200

    fix(client): app registration was fucked up

--- src/client/add_application/index.ts
+++ src/client/add_application/index.ts
@@ -4,10 +4,15 @@ import { appConfig } from './data';
 const lbPhone = 'lb-phone';
 
 const loadApplication = () => {
-  const [added, errorMessage] = exports[lbPhone].AddCustomApp(appConfig) as [boolean, string?];
+
+  const lbPhoneExports = global.exports[lbPhone];
+
+  const response = lbPhoneExports.AddCustomApp(appConfig) as [boolean, string?];
+
+  const added = Array.isArray(response) ? response[0] : response;
 
   if (!added) {
-    console.log(`[^1ERROR^7] Unable to add "^5${appConfig.name}^7" to lb-phone: ${errorMessage}`);
+    console.log(`[^1ERROR^7] Unable to add "^5${appConfig.name}^7" to lb-phone: ${response[1]}`);
   }
 };
 
