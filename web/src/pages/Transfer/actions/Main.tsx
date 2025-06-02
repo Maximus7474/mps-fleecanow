312a1f07e21a996cfa9905819661034319f10404
Maximus7474 <the.maximus.7474@gmail.com>
2025-06-02 23:39:54 +0200

    tweak(Transfer/Main):icon size

--- web/src/pages/Transfer/actions/Main.tsx
+++ web/src/pages/Transfer/actions/Main.tsx
@@ -7,11 +7,11 @@ const TransferTypeSelection: React.FC<TransferProps> = ({ setSection }) => {
       <p>How do you want to send money ?</p>
       <div className='actions'>
         <button onClick={() => setSection('username')}>
-          <User size={128} />
+          <User size={96} />
           <h3>By username</h3>
         </button>
         <button onClick={() => setSection('proximity')}>
-          <Radio size={128} />
+          <Radio size={96} />
           <h3>By proximity</h3>
         </button>
       </div>
