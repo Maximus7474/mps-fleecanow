123acb92af17a3191bdae95657b586accdeaaf6f
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-10 01:10:43 +0200

    feat(client/account): nui callbacks for handling fund transfers

---
+++ src/client/account/funds.ts
@@ -0,0 +1,18 @@
+import { GetBalanceResponse } from '@common/types';
+import { triggerServerCallback } from '../utils/callbacks';
+
+RegisterNuiCallback(
+  'fleecanow:getBalance',
+  async (_: null, cb: (data: GetBalanceResponse) => void) => {
+    const response: GetBalanceResponse = await triggerServerCallback('fleecanow:getBalance');
+    cb(response);
+  },
+);
+
+RegisterNuiCallback(
+  'fleecanow:handleFunds',
+  async (data: { action: 'withdraw' | 'add'; amount: number }, cb: (data: GetBalanceResponse) => void) => {
+    const response: GetBalanceResponse = await triggerServerCallback('fleecanow:handleFunds', data);
+    cb(response);
+  },
+);
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,4 @@
 import './add_application';
 import './account/account';
+import './account/funds';
 import './transfer';
