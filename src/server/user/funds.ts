e01071f5e6c2e87c575230bd2efe0cc4a82ad68c
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-10 01:12:14 +0200

    feat(server/user): WIP server callbacks for handling funds

---
+++ src/server/user/funds.ts
@@ -0,0 +1,25 @@
+import { GetBalanceResponse } from '@common/types';
+import { RegisterServerCallback } from '../utils/callbacks';
+import { resourceExport } from '@common/export';
+
+RegisterServerCallback(
+  'fleecanow:getBalance',
+  async (source: number): Promise<GetBalanceResponse> => {
+    try {
+      const result: number = resourceExport('mps-lb-fleecanow', 'GetBankBalance')(source);
+
+      return { success: true, amount: result };
+    } catch (err) {
+      console.error(err);
+      return { success: false, error: 'Unable to fetch balance' };
+    }
+  },
+);
+
+RegisterServerCallback(
+  'fleecanow:handleFunds',
+  async (source: number, data: { action: 'withdraw' | 'add'; amount: number }): Promise<GetBalanceResponse> => {
+    console.log('fleecanow:handleFunds', source, data)
+    return { success: true, amount: 69 };
+  },
+);
