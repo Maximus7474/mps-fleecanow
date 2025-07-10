ad19dc6928da6e4a389c710416e9e13c5c729c44
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-10 13:19:14 +0200

    feat(server): 5 minute interval to save all account balance

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,6 @@
 import './user/auth';
+import { FleecaNowUser } from './user/class';
 import './user/funds';
+
+// Save all players balance every 5 minutes
+setInterval(FleecaNowUser.save, 5 * 60_000);
