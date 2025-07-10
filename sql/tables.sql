0d7a26aec8c2095a51ff3a90749feadf9734f4f7
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-10 11:24:01 +0200

    feat(sql): base tables

---
+++ sql/tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `phone_fleecanow_accounts` (
+  `id`                int(11)       NOT NULL AUTO_INCREMENT,
+  `username`          varchar(50)   NOT NULL,
+  `display_name`      varchar(50)   DEFAULT NULL,
+  `email`             varchar(50)   DEFAULT NULL,
+  `avatar`            longtext      DEFAULT NULL,
+  `password`          varchar(255)  NOT NULL,
+  `proximity_sharing` tinyint(4)    NOT NULL DEFAULT 0,
+  `balance`           int(11)       NOT NULL DEFAULT 0,
+  
+  PRIMARY KEY (`username`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
