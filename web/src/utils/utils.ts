23763f1cf8f62f1ff4f96b42a24bf86e81793055
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-22 01:18:24 +0200

    refactor(web/utils): doubt it'll go that high, but who knows with current 5M servers, added billion format & improved readability of formatBalanceValue(value)

--- web/src/utils/utils.ts
+++ web/src/utils/utils.ts
@@ -1,10 +1,12 @@
 export const devMode = !window?.['invokeNative'];
 
 export const formatBalanceValue = (value: number) => {
-  if (value > 1000000) {
-    return `${parseFloat((value / 1000000).toFixed(1))}M `;
-  } else if (value > 1000) {
-    return `${parseFloat((value / 1000).toFixed(1))}k `;
+  if (value > 1_000_000_000) {
+    return `${parseFloat((value / 1_000_000_000).toFixed(1))}B `;
+  } else if (value > 1_000_000) {
+    return `${parseFloat((value / 1_000_000).toFixed(1))}M `;
+  } else if (value > 1_000) {
+    return `${parseFloat((value / 1_000).toFixed(1))}k `;
   }
   return `${value} `;
 };
