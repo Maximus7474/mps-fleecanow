98b1a53581363812eea3bb05722d6d4c0e6a2427
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-22 00:44:55 +0200

    refactor(server/log_account_action): added some input validation & track message for transfers

--- src/server/utils/log_account_action.ts
+++ src/server/utils/log_account_action.ts
@@ -5,9 +5,18 @@ export async function LogAccountAction(data: {
   action: 'transfer' | 'withdraw' | 'deposit';
   amount: number;
   related_account: number | null;
+  message: string | null;
 }): Promise<void> {
+  const related_account = data.action === 'transfer'
+    ? data.related_account
+    : null;
+
+  const message = data.message && data.action === 'transfer' && data.message.length > 0
+    ? data.message
+    : null;
+
   await MySQL.insert(
-    'INSERT INTO `phone_fleecanow_transfers` (`account`, `action`, `amount`, `related_account`) VALUES (?, ?, ?, ?)',
-    [data.account, data.action, data.amount, data.related_account ?? null],
+    'INSERT INTO `phone_fleecanow_transfers` (`account`, `action`, `amount`, `related_account`, `message`) VALUES (?, ?, ?, ?, ?)',
+    [data.account, data.action, data.amount, related_account, message],
   );
 }
