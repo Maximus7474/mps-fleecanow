af17f51b59ca8b536f9dd94ed81da10df784c3e1
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-03 00:33:20 +0200

    refactor(common/locale): removes the ox_lib dependency

--- src/common/locale.ts
+++ src/common/locale.ts
@@ -1,11 +1,47 @@
-import { locale, type FlattenObjectKeys } from '@overextended/ox_lib';
+import { LoadFile } from './utils';
+
+// https://www.raygesualdo.com/posts/flattening-object-keys-with-typescript-types/
+export type FlattenObjectKeys<T extends Record<string, any>, Key = keyof T> = Key extends string
+  ? T[Key] extends Record<string, unknown>
+  ? `${Key}.${FlattenObjectKeys<T[Key]>}`
+  : `${Key}`
+  : never;
 
 type RawLocales = FlattenObjectKeys<typeof import('../../locales/en.json')>;
 
-function Locale<T extends RawLocales>(str: T, ...args: any[]): string;
-function Locale<T extends string>(str: T, ...args: any[]): string | unknown;
-function Locale<T extends string>(str: T, ...args: any[]) {
-  return locale(str, ...args);
+const localeData = LoadFile('locales/en.json');
+
+const Locale = (
+  key: RawLocales,
+  args: {[key: string]: string | number} = {}
+) => {
+  const keyParts = key.split('.');
+  let currentLocale = localeData;
+  let found = true;
+
+  for (const part of keyParts) {
+    if (currentLocale && typeof currentLocale === 'object' && currentLocale.hasOwnProperty(part)) {
+      currentLocale = currentLocale[part];
+    } else {
+      found = false;
+      break;
+    }
+  }
+
+  if (!found || typeof currentLocale !== 'string') {
+    return key;
+  }
+
+  let localizedString = currentLocale;
+
+  for (const argKey in args) {
+    if (args.hasOwnProperty(argKey)) {
+      const regex = new RegExp(`\\{${argKey}\\}`, 'g');
+      localizedString = localizedString.replace(regex, String(args[argKey]));
+    }
+  }
+
+  return localizedString;
 }
 
 export default Locale;
