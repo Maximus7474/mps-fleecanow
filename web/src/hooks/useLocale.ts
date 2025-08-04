4f20b1fd69c259fbce9e18bacda119d1e4e38238
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-04 23:42:39 +0200

    feat(web/hooks): useLocale function

---
+++ web/src/hooks/useLocale.ts
@@ -0,0 +1,19 @@
+import { createContext, useContext } from 'react';
+import { type FlattenObjectKeys } from '@common/locale'
+
+type LocaleContextType = {
+  T: (key: string, args?: { [key: string]: string|number }) => string;
+  UpdateLocale: (locale: string) => void;
+};
+
+export type RawLocales = FlattenObjectKeys<typeof import('../../../locales/en.json')>;
+
+export const LocaleContext = createContext<LocaleContextType | undefined>(undefined);
+
+export function useLocale() {
+  const context = useContext(LocaleContext);
+  if (!context) {
+    throw new Error('useLocale must be used within an LocaleProvider');
+  }
+  return context;
+}
