8724ed17582ef20218eb18aef793a360c63b10b4
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-10 12:59:44 +0200

    chore(server/funds): use locale function for text

--- src/server/user/funds.ts
+++ src/server/user/funds.ts
@@ -1,17 +1,18 @@
 import { GetBalanceResponse } from '@common/types';
 import { RegisterServerCallback } from '../utils/callbacks';
 import { FleecaNowUser } from './class';
+import Locale from '@common/locale';
 
 RegisterServerCallback('fleecanow:getBalance', async (source: number): Promise<GetBalanceResponse> => {
   try {
     const user: FleecaNowUser = FleecaNowUser.getUserBySource(source);
 
-    if (!user) return { success: false, error: 'Unable to fetch balance' };
+    if (!user) return { success: false, error: Locale('CORE.FUNDS.UNABLE_TO_GET_BALANCE') };
 
     return { success: true, amount: user.get('balance') as number };
   } catch (err) {
     console.error(`Unable to get player (svid: ${source}) balance`, err);
-    return { success: false, error: 'Unable to fetch balance' };
+    return { success: false, error: Locale('CORE.FUNDS.UNABLE_TO_GET_BALANCE') };
   }
 });
 
@@ -20,7 +21,7 @@ RegisterServerCallback(
   async (source: number, data: { action: 'withdraw' | 'add'; amount: number }): Promise<GetBalanceResponse> => {
     const user: FleecaNowUser = FleecaNowUser.getUserBySource(source);
 
-    if (!user) return { success: false, error: 'Unable to fetch balance' };
+    if (!user) return { success: false, error: Locale('CORE.FUNDS.UNABLE_TO_GET_BALANCE') };
 
     return user.updateFunds(data.action, data.amount);
   },
