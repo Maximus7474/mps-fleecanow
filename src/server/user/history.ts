dca80e1bc5fe03b80dc69851de22b64153470baf
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-10 13:00:41 +0200

    chore(server/history): use locale function for text

--- src/server/user/history.ts
+++ src/server/user/history.ts
@@ -1,18 +1,19 @@
 import { HistoryResponse } from '@common/types';
 import { FleecaNowUser } from './class';
 import { RegisterServerCallback } from '../utils/callbacks';
+import Locale from '@common/locale';
 
 RegisterServerCallback('fleecanow:getHistory', async (source: number): Promise<HistoryResponse> => {
   try {
     const user: FleecaNowUser = FleecaNowUser.getUserBySource(source);
 
-    if (!user) return { success: false, error: 'Unable to fetch balance' };
+    if (!user) return { success: false, error: Locale('CORE.HISTORY.UNABLE_TO_GET_HISTORY') };
 
     const history = await user.getHistory();
 
     return { success: true, history };
   } catch (err) {
-    console.error(`Unable to get player (svid: ${source}) balance`, err);
-    return { success: false, error: 'Unable to fetch balance' };
+    console.error(`Unable to get player (svid: ${source}) history`, err);
+    return { success: false, error: Locale('CORE.HISTORY.UNABLE_TO_GET_HISTORY') };
   }
 });
