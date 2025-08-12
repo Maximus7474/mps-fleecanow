4bf997f640d86655d7dd08066c03262aa855c120
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-12 22:26:27 +0200

    feat(server): logging wrapper

---
+++ src/server/utils/logging_wrapper.ts
@@ -0,0 +1,10 @@
+export function Log(
+  level: 'info' | 'error' | 'success',
+  action: 'deposit_funds' | 'withdraw_funds' | 'transfer',
+  title: string,
+  fields: Record<string, any> | null,
+  source: string | null,
+  target: string | null
+): void {
+  exports['mps-fleecanow'].Log(level, action, title, fields, source, target)
+}
