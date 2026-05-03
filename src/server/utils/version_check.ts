92d3f4ef2fcc687fcd5d6267147e2cbe7c5e667f
Maximus7474 <the.maximus.7474@gmail.com>
2026-05-03 13:17:08 +0200

    refactor(server/utils): changed version check system

--- src/server/utils/version_check.ts
+++ src/server/utils/version_check.ts
@@ -1,51 +1,35 @@
-import Config from '@common/config';
+// import Config from '@common/config';
 
 export async function VersionCheck() {
-  if (Config.VersionCheck !== false) return;
+  // if (Config.VersionCheck !== false) return;
 
   let repo_version: string = '0.0.0';
-  const current_version: string = GetResourceMetadata(GetCurrentResourceName(), 'version', 0);
+  const current_version: string = '0.5.5'; // GetResourceMetadata(GetCurrentResourceName(), 'version', 0);
 
   try {
     const versionResponse = await fetch(
-      'https://raw.githubusercontent.com/Maximus7474/Maximus7474/main/data/fleecanow/version',
+      'https://api.github.com/repos/Maximus7474/mps-fleecanow/releases/latest',
     );
 
     if (!versionResponse.ok) {
       throw new Error(`HTTP error! status: ${versionResponse.status}`);
     }
 
-    const responseText = await versionResponse.text();
-    if (responseText !== null && responseText !== '') {
-      repo_version = responseText.replace(/\n/g, '');
-    }
+    const responseData = await versionResponse.json() as any;
+    const upstreamVersion = responseData.tag_name as string;
+
+    repo_version = upstreamVersion.replace(/[a-zA-Z]+/, '');
   } catch (error) {
     console.error(`^1Failed to get latest version:^7`, error);
     repo_version = '0.0.0';
   }
 
   if (repo_version !== current_version) {
-    try {
-      const notesResponse = await fetch(
-        'https://raw.githubusercontent.com/Maximus7474/Maximus7474/main/data/fleecanow/notes',
-      );
-
-      if (!notesResponse.ok) {
-        throw new Error(`HTTP error! status: ${notesResponse.status}`);
-      }
-
-      const updateNotes = await notesResponse.text();
-
-      console.log('^6------------------------------------------------------------------------------^7');
-      console.log(`^4^3mps-fleecanow^4 has a new update available (^2${current_version}^4 -> ^2${repo_version}^4):^7`);
-      console.log(`^4${updateNotes}`);
-      console.log('^6------------------------------------------------------------------------------^7');
-    } catch (error) {
-      console.error(`^1Failed to get fleecanow update notes:^7`, error);
-      console.log(`^5Unable to fetch update notes at this time.^7`);
-      console.log('^6------------------------------------------------------------------------------^7');
-    }
-  } else {
-    console.log(`^2Running latest version^7`);
+    console.log('^6------------------------------------------------------------------------------^7');
+    console.log(`^4^3mps-fleecanow^4 has a new update available (^2${current_version}^4 -> ^2${repo_version}^4):^7`);
+    console.log(`^4 -> https://github.com/Maximus7474/mps-fleecanow/releases/latest^7`);
+    console.log('^6------------------------------------------------------------------------------^7');
   }
 }
+
+VersionCheck();
