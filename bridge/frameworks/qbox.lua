9204b30a678cd145719619f4fce9f5ba347e4451
Maximus7474 <the.maximus.7474@gmail.com>
2026-05-08 00:10:52 +0200

    fix(bridge/qbox): use exports instead of old inherited qb methods

--- bridge/frameworks/qbox.lua
+++ bridge/frameworks/qbox.lua
@@ -4,13 +4,9 @@ if (not IsFrameworkStarted("qbx")) then return end
 ---@param src number
 ---@return integer
 function GetBankBalance(src)
-    local qPlayer = exports.qbx_core:GetPlayer(tonumber(src))
+    local amount = exports.qbx_core:GetMoney(src, 'bank')
 
-    if not qPlayer then
-        return 0
-    end
-
-    return qPlayer.Functions.GetMoney("bank") or 0
+    return amount or 0
 end
 
 ---Remove money from a player's bank account
@@ -18,23 +14,9 @@ end
 ---@param amount number
 ---@return boolean success
 function RemoveMoney(src, amount)
-    local qPlayer = exports.qbx_core:GetPlayer(tonumber(src))
+    local result = exports.qbx_core:RemoveMoney(src, 'bank', amount, Locale("REMOVED_MONEY"))
 
-    if not qPlayer then
-        return false
-    end
-
-    local balance = GetBankBalance(src)
-
-    if (balance < amount) then
-        return false
-    end
-
-    local result = qPlayer.Functions.RemoveMoney("bank", amount, Locale("REMOVED_MONEY"))
-
-    print('RemoveMoney qbox result', result or 'nil')
-
-    return true
+    return result
 end
 
 ---Add money to a player's bank account
@@ -42,15 +24,9 @@ end
 ---@param amount number
 ---@return boolean success
 function AddMoney(src, amount)
-    local qPlayer = exports.qbx_core:GetPlayer(tonumber(src))
+    local result = exports.qbx_core:RemoveMoney(src, 'bank', amount, Locale("ADDED_MONEY"))
 
-    if not qPlayer or amount < 0 then
-        return false
-    end
-
-    qPlayer.Functions.AddMoney("bank", amount, Locale("ADDED_MONEY"))
-
-    return true
+    return result
 end
 
 exports('GetBankBalance', GetBankBalance)
